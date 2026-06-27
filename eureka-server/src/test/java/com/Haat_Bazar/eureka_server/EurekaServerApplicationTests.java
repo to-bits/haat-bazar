ef23beac039acc7be121d2b74bef5b3e3143565a
ihfaz297 <adib000297@gmail.com>
2026-06-27 21:44:19 +0600

    feat: add eureka-server and api-gateway

---
+++ eureka-server/src/test/java/com/Haat_Bazar/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Haat_Bazar.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
