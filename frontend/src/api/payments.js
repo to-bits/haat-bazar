0e8928c91faeb91d93ea1dbc24be8d8ff2ee7d68
Sajib Kumar Roy <sajibr917@gmail.com>
2026-06-28 11:56:30 +0600

    Initial commit

--- frontend/src/api/payments.js
+++
@@ -1,7 +0,0 @@
-import client from './client';
-
-export const paymentApi = {
-  pay: (data) => client.post('/payments', data).then((r) => r.data),
-  getByOrder: (orderId) =>
-    client.get(`/payments/order/${orderId}`).then((r) => r.data),
-};
